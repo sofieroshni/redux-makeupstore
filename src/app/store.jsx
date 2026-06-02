de5059879b553091ed8dc5ec56a709fab95ce280
Sofie <sov.002@edu.zealand.dk>
2026-06-02 20:39:07 +0200

    stier i store.jsx rettet

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../Features/cartSlice';
+import cartReducer from '../Features/cartSlice.jsx';
 // import userReducer from './userSlice';
-import productsReducer from '../Features/productsSlice';
+import productsReducer from '../Features/ProductsSlice.jsx';
 
 export const store = configureStore({
   reducer: {
